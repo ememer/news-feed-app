12b82c492d3080cc3e4a261a4a9888e5696d4154
ememer <46853050+ememer@users.noreply.github.com>
2022-12-23 21:03:21 +0100

    UI updates, connect basic fetch

--- src/components/NewsFeed.tsx
+++ src/components/NewsFeed.tsx
@@ -1,24 +1,38 @@
-import React from 'react';
+import { useEffect, useState } from 'react';
 
 import { layoutTheme } from '../shared/theme/LayoutTheme';
+import { ResponseArray } from '../types/NewsFeedArticleType';
 import NewsFeedCard from './NewsFeedCard';
 
 const NewsFeed = () => {
+  const [response, setResponse] = useState<ResponseArray>();
   const theme = layoutTheme[0];
+
+  const TOKEN = 'apiKey=dcfea20b502345c6be30e1d013d3d7b3';
+  const URL = 'https://newsapi.org/v2/everything?' + 'q=Sport&' + TOKEN;
+  const request: Request = new Request(URL);
+
+  const news = async (): Promise<ResponseArray> => {
+    const resp = await fetch(request);
+
+    if (!resp.ok) {
+      const message = `Error exist ${resp.status}`;
+      throw new Error(message);
+    }
+
+    const articlesResponse = await resp.json();
+    setResponse(articlesResponse);
+    return articlesResponse;
+  };
+  useEffect(() => {
+    news().catch((err) => err.message);
+  }, []);
 
   return (
     <div className="grid gap-10 ml-auto mt-20 p-10 grid-cols-1 md:grid-cols-2 xl:grid-cols-3 w-full scroll-smooth scroll-m-10 lg:w-10/12">
-      <NewsFeedCard theme={theme} />
-      <NewsFeedCard theme={theme} />
-      <NewsFeedCard theme={theme} />
-      <NewsFeedCard theme={theme} />
-      <NewsFeedCard theme={theme} />
-      <NewsFeedCard theme={theme} />
-      <NewsFeedCard theme={theme} />
-      <NewsFeedCard theme={theme} />
-      <NewsFeedCard theme={theme} />
-      <NewsFeedCard theme={theme} />
-      <NewsFeedCard theme={theme} />
+      {response?.articles.map((article, idx) => (
+        <NewsFeedCard key={`${article.title}+${idx}`} article={article} theme={theme} />
+      ))}
     </div>
   );
 };
--- src/components/NewsFeedCard.tsx
+++ src/components/NewsFeedCard.tsx
@@ -1,33 +1,116 @@
+import {
+  faHeart,
+  faNewspaper,
+  faShareFromSquare,
+} from '@fortawesome/free-regular-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { clsx } from 'clsx';
-import React from 'react';
+import { useState } from 'react';
 
-import { clipLongText, dumpyText } from '../shared/utils/clipText';
+import { clipLongText } from '../shared/utils/clipText';
 import { LayoutTheme } from '../types/layoutTheme';
+import { ArticleResponse } from '../types/NewsFeedArticleType';
 interface Props {
   theme: LayoutTheme;
+  article: ArticleResponse;
 }
 
-const NewsFeedCard = ({ theme }: Props) => {
+const NewsFeedCard = ({ theme, article }: Props) => {
+  const { source, title, author, url, urlToImage, content } = article;
+  const [voteCount, setVoteCount] = useState({
+    vote: 0,
+    isClicked: false,
+  });
+  const [share, setShare] = useState(false);
+
+  const addVote = () => {
+    setVoteCount((prev) => ({
+      vote: prev.vote + 1,
+      isClicked: true,
+    }));
+    setTimeout(() => {
+      setVoteCount((prev) => ({
+        ...prev,
+        isClicked: false,
+      }));
+    }, 400);
+  };
+
   return (
-    <div
+    <article
       className={clsx(
-        'w-full rounded-xl snap-always snap-center border shadow-lg cursor-pointer hover:scale-105 ease-in-out duration-200 hover:rotate-1  overflow-hidden',
+        'flex flex-col justify-around w-full rounded-xl snap-always snap-center border shadow-lg cursor-pointer hover:scale-105 ease-in-out duration-200 transition-transform overflow-hidden group/cardItem',
         theme.elementsLinearBG,
         theme.borderB,
         theme.mainText,
       )}
     >
-      <div className="p-4 pb-0h-3/4">
-        <h2 className="font-bold text-2xl w-full my-2">Title</h2>
-        <span className="my-2 font-light">author</span>
-        <p className="my-4">{clipLongText(dumpyText, 70)}</p>
+      <div className="grid grid-cols-2 items-center justify-between p-4">
+        <span className={clsx(theme.mainAccText)}>
+          <FontAwesomeIcon className="mr-2" icon={faNewspaper} />
+          {source?.id?.toLocaleUpperCase()}
+        </span>
+        <a
+          className={clsx(
+            'invisible group-hover/cardItem:visible text-center group-hover/cardItem:border rounded-md p-2',
+            theme.borderB,
+            theme.elementsLinearBG,
+          )}
+          href={url}
+          target="_blank"
+          rel="noreferrer"
+        >
+          Open in new Tab
+        </a>
+      </div>
+      <div className="p-4 pb-0 min-h-10-s mb-4">
+        <h2 className="font-bold text-2xl w-full my-2">{title}</h2>
+        <span className="my-2 font-light">{author}</span>
+        <p className="my-4">{clipLongText(content, 100)}</p>
       </div>
       <img
-        className="object-cover aspect-video h-full"
-        alt="ALT TEMPORARY"
-        src="https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg"
+        className="object-cover aspect-video rounded-b-md"
+        alt={`${title} article`}
+        src={urlToImage ?? 'https://miro.medium.com/max/800/1*hFwwQAW45673VGKrMPE2qQ.png'}
       />
-    </div>
+      <div className="p-4 flex row justify-between">
+        <div className="flex flex-row items-center">
+          <button className="text-2xl" onClick={() => addVote()}>
+            <FontAwesomeIcon icon={faHeart} />
+          </button>
+          <span
+            className={clsx(
+              voteCount.isClicked && theme.mainAccText,
+              'ml-4 duration-75 transform ease-in-out',
+            )}
+          >
+            {voteCount.vote !== 0 ? voteCount.vote : null}
+          </span>
+        </div>
+        <div className="flex flex-row items-center overflow-hidden">
+          <button
+            className="text-2xl"
+            onClick={() => {
+              setShare(true);
+              // TODO
+
+              // Set url to clipboard
+
+              setTimeout(() => {
+                setShare(false);
+              }, 2000);
+            }}
+          >
+            <FontAwesomeIcon icon={faShareFromSquare} />
+          </button>
+          {share && (
+            <span className={clsx(share && 'ml-2', theme.mainAccText)}>
+              Link saved in clipboard
+            </span>
+          )}
+        </div>
+      </div>
+    </article>
   );
 };
 
