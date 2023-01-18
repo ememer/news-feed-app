786cdd05987963341787c6091b4fe83a42ebd09d
ememer <46853050+ememer@users.noreply.github.com>
2023-01-18 11:44:05 +0100

    Fixed button event on cards

--- src/components/NewsFeedCard.tsx
+++ src/components/NewsFeedCard.tsx
@@ -92,77 +92,77 @@ const NewsFeedCard = ({ theme, article, onClick, index, isReference }: Props) =>
   return (
     <article
       ref={isReference}
-      onClick={(e) => {
-        if ((e.target as HTMLDivElement).id !== 'UIelement') {
-          setFillComponentData({
-            componentId: index,
-            voteReactionCount: userReactions.vote as number,
-            messagesReactionCount: userReactions.messages as number,
-          });
-          onClick(true);
-        }
-      }}
       className={clsx(
-        'group/cardItem flex w-full cursor-pointer snap-center snap-always flex-col justify-around overflow-hidden rounded-xl border shadow-lg transition-transform duration-200 ease-in-out hover:scale-105',
+        'group/cardItem flex w-full  snap-center snap-always flex-col justify-around overflow-hidden rounded-xl border shadow-lg transition-transform duration-200 ease-in-out hover:scale-105',
         theme.elementsLinearBG,
         theme.borderB,
         theme.mainText,
       )}
     >
-      <div id="article" className="grid grid-cols-2 items-center justify-between p-4">
-        <span className={clsx('text-xs lg:text-sm', theme.mainAccText)}>
-          <FontAwesomeIcon className="mr-2" icon={faNewspaper} />
-          {source_id?.toLocaleUpperCase()}
-        </span>
-        <a
-          id="UIelement"
-          className={clsx(
-            'rounded-md border p-2 text-center  lg:invisible lg:group-hover/cardItem:visible',
-            theme.borderB,
-            theme.elementsLinearBG,
+      <div
+        className="cursor-pointer"
+        onClick={(e) => {
+          if ((e.target as HTMLDivElement).id !== 'UIelement') {
+            setFillComponentData({
+              componentId: index,
+              voteReactionCount: userReactions.vote as number,
+              messagesReactionCount: userReactions.messages as number,
+            });
+            onClick(true);
+          }
+        }}
+      >
+        <div id="article" className="grid grid-cols-2 items-center justify-between p-4">
+          <span className={clsx('text-xs lg:text-sm', theme.mainAccText)}>
+            <FontAwesomeIcon className="mr-2" icon={faNewspaper} />
+            {source_id?.toLocaleUpperCase()}
+          </span>
+          <a
+            id="UIelement"
+            className={clsx(
+              'rounded-md border p-2 text-center  lg:invisible lg:group-hover/cardItem:visible',
+              theme.borderB,
+              theme.elementsLinearBG,
+            )}
+            href={link as string}
+            target="_blank"
+            rel="noreferrer"
+          >
+            {t('newTab')}
+          </a>
+        </div>
+        <div className={clsx('flex flex-wrap p-4 font-semibold', theme.textP)}>
+          {publishedDate.toLocaleDateString()}
+        </div>
+        <div className="mb-4 min-h-10-s p-4 pb-0">
+          <h2 className="my-2 w-full text-2xl font-bold">{title}</h2>
+          <span className="my-2 font-light">{creator ?? ''}</span>
+          {!description && !content ? (
+            <p className="my-4">{'No article description available'}</p>
+          ) : (
+            <p className="my-4">
+              {clipLongText((description ?? content) as string, 100)}
+            </p>
           )}
-          href={link as string}
-          target="_blank"
-          rel="noreferrer"
-        >
-          {t('newTab')}
-        </a>
-      </div>
-      <div className={clsx('flex flex-wrap p-4 font-semibold', theme.textP)}>
-        {publishedDate.toLocaleDateString()}
-      </div>
-      <div className="mb-4 min-h-10-s p-4 pb-0">
-        <h2 className="my-2 w-full text-2xl font-bold">{title}</h2>
-        <span className="my-2 font-light">{creator ?? ''}</span>
-        {!description && !content ? (
-          <p className="my-4">{'No article description available'}</p>
-        ) : (
-          <p className="my-4">{clipLongText((description ?? content) as string, 100)}</p>
-        )}
-      </div>
+        </div>
 
-      <LazyLoad threshold={0.5}>
-        <img
-          className="aspect-video rounded-b-md object-cover"
-          alt={title as string}
-          src={image_url ?? missingImage}
-        />
-      </LazyLoad>
-      <div id="UIelement" className="row flex justify-between p-4">
+        <LazyLoad threshold={0.5}>
+          <img
+            className="mx-auto aspect-video rounded-b-md object-cover"
+            alt={title as string}
+            src={image_url ?? missingImage}
+          />
+        </LazyLoad>
+      </div>
+      <div className="row flex justify-between p-4">
         <button
           className={clsx('flex flex-row items-center', theme.hoverP)}
           title={t('voteUpButton') as string}
-          id="UIelement"
           onClick={() => addVote()}
         >
-          <div id="UIelement">
-            <FontAwesomeIcon
-              id="UIelement"
-              className="bg-transparent text-2xl"
-              icon={faHeart}
-            />
+          <div>
+            <FontAwesomeIcon className="bg-transparent text-2xl" icon={faHeart} />
             <span
-              id="UIelement"
               className={clsx(
                 userReactions.isClicked && theme.mainAccText,
                 'ml-4 transform text-base',
@@ -186,7 +186,6 @@ const NewsFeedCard = ({ theme, article, onClick, index, isReference }: Props) =>
         <button
           className={clsx('flex flex-row items-center overflow-hidden', theme.hoverP)}
           title={t('shareButton') as string}
-          id="UIelement"
           onClick={() =>
             navigator.share({
               title: title as string,
@@ -195,8 +194,8 @@ const NewsFeedCard = ({ theme, article, onClick, index, isReference }: Props) =>
             })
           }
         >
-          <div id="UIelement" className="text-2xl">
-            <FontAwesomeIcon id="UIelement" icon={faShareFromSquare} />
+          <div className="bg-transparent text-2xl">
+            <FontAwesomeIcon icon={faShareFromSquare} />
           </div>
         </button>
       </div>
